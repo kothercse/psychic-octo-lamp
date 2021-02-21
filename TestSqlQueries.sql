1388b37181e49dc04ccbec111445e887f81a2e55
kothercse <69390701+kothercse@users.noreply.github.com>
2021-02-21 17:27:23 +0530

    Add files via upload

---
+++ TestSqlQueries.sql
@@ -0,0 +1,9 @@
+use Employee
+
+insert into EmployeeForm1(Code,Department)values('C3D4','sales'),('E5F6','Accounting');
+
+select * from EmployeeForm1;
+
+insert into EmployeeForm2(Code,Name)values('2001','Kother'),('2002','Niyasali'),('2003','Hamsha'),('2004','Abdul');
+
+select * from EmployeeForm2
